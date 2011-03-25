2858ef663321e6af07413a6a5c2a22a613b6a05d
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-03-25 13:33:57 +0900

    install Symfonyan

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Tumf\TepcoBundle\TumfTepcoBundle(),
             new Bundle\ZendCacheBundle\ZendCacheBundle(),
+            new Acme\SymfonyanBundle\SymfonyanBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Acme/SymfonyanBundle/SymfonyanBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Acme\SymfonyanBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymfonyanBundle extends Bundle
+{
+
+}
